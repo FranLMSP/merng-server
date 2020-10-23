58161aaec9416a6145d112f481bad9f1c8aa07d8
Franco Colmenarez <francoacg1@gmail.com>
2020-10-23 18:04:38 -0500

    Auth middleware and create/delete posts

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const { MONGODB } = require('./config.js')
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: ({req}) => ({req})
 })
 
 mongoose.connect(MONGODB, { useUnifiedTopology: true, useNewUrlParser: true})
